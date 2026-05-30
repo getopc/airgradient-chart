6e8e7620bbd897067d9efe7eb6264fd23a69bc5f
getopc <57887833+getopc@users.noreply.github.com>
2026-05-30 11:30:10 +0900

    Update ai_report.py

--- air/pages/ai_report.py
+++ air/pages/ai_report.py
@@ -61,6 +61,7 @@ def run_ai_analysis(air_data):
 
     recent_data = air_data.tail(30)
     st.write(air_data)
+    recent_data = air_data.tail(30)
     data_summary = recent_data.describe().to_string()
 
     prompt = f"""
